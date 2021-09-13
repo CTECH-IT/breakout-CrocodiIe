85b554bab4d041cb05d1a656c88d2f809ac7b907
Bradley Grupczynski <8422702@isd535.org>
2021-09-13 09:15:16 -0500

    NEW CMT: Created canvas and added three shapes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
-// Put your JavaScript here
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+// This prints the square(s)
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+//rectangle with stroke
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
+ctx.closePath();
+
+//The ball
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "Green";
+ctx.fill();
+ctx.closePath();
